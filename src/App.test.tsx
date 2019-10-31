0806e768dfede73af1e41041a99f4a9ddeb2d326
Claudio Ricci <claudioricci@Claudios-MBP.home>
2019-10-31 23:16:19 +0000

    App component mounting and rendering tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { unmountComponentAtNode } from "react-dom";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+let container: any = null;
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+beforeEach(() => {
+  // Setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // Cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
 });
